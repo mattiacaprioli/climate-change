14b4dad41bc0679a931a102ac6f5d60c7b232f3b
Mattia Caprioli <capriolimattia1994@gmail.com>
2023-10-06 21:03:56 +0200

    sistemato navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-black shadow-lg fixed top-0 w-full">
+    <nav className="bg-black shadow-lg fixed top-0 w-full z-10">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
           <div className="flex-shrink-0 flex items-center">
